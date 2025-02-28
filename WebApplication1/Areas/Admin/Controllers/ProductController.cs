8d072fd88a08ac7549a23cc10eaa114043de0f43
TanMinh-MCN <fctanminh0304@gmail.com>
2025-03-01 00:22:32 +0700

    fix udpate product

--- WebApplication1/Areas/Admin/Controllers/ProductController.cs
+++ WebApplication1/Areas/Admin/Controllers/ProductController.cs
@@ -19,6 +19,20 @@ namespace WebApplication1.Areas.Admin.Controllers
             _unitOfWork = unitOfWork;
             _webHostEnvironment = webHostEnvironment;
         }
+
+        public string handleUploadFile(IFormFile file)
+        {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            string productPath = Path.Combine(wwwRootPath + @"/images/product");
+
+            using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+            return @"/images/product/" + fileName;
+        }
+
         public IActionResult Index()
         {
             List<Product> listProduct = _unitOfWork.productRepository.GetAll().ToList();
@@ -55,18 +69,18 @@ namespace WebApplication1.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                string wwwRootPath = _webHostEnvironment.WebRootPath;
                 if(file != null)
                 {
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                    string productPath = Path.Combine(wwwRootPath + @"/images/product");
+                    //string wwwRootPath = _webHostEnvironment.WebRootPath;
+                    //string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    //string productPath = Path.Combine(wwwRootPath + @"/images/product");
 
-                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
-                    {
-                        file.CopyTo(fileStream);
-                    }
+                    //using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    //{
+                    //    file.CopyTo(fileStream);
+                    //}
 
-                    productVM.Product.ImageUrl = @"/images/product/" + fileName;
+                    productVM.Product.ImageUrl = handleUploadFile(file);
                 }
                 _unitOfWork.productRepository.Add(productVM.Product);
                 _unitOfWork.Save();
@@ -83,28 +97,51 @@ namespace WebApplication1.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            Product ProductObj = _unitOfWork.productRepository.Get(o => o.ID == id);
+            Product productObj = _unitOfWork.productRepository.Get(o => o.ID == id);
 
-            if (ProductObj == null)
+            if (productObj == null)
             {
                 return NotFound();
             }
 
-            return View(ProductObj);
+            ProductVM productVM = new()
+            {
+                Product = productObj,
+                CategoryList = _unitOfWork.categoryRepository.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.ID.ToString()
+                })
+            };
+
+            return View(productVM);
         }
 
         // HTTP - POST
         [HttpPost]
-        public IActionResult Edit(Product obj)
+        public IActionResult Edit(Product product, IFormFile? file)
         {
-            if (obj == null)
+            if (product == null)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.productRepository.Update(obj);
+                if (file != null)
+                {
+                    // Delete the existing image
+                    string oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+
+                    // Assign a new image path
+                    product.ImageUrl = handleUploadFile(file);
+                }
+
+                _unitOfWork.productRepository.Update(product);
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
             }
---
+++ WebApplication1/Utility/Helper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1.Utility
+{
+    public class Helper
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public Helper(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public string handleUploadFile(IFormFile file)
+        {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            string productPath = Path.Combine(wwwRootPath + @"/images/product");
+
+            using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return fileName;
+        }
+    }
+}
